d00cbd4f27e474736c12cdff234883961b46442e
gracethom <gracethomas@Gracies-MacBook-Pro.local>
2015-06-07 18:07:00 -0400

    added query for location datda
    still need to fix while loop to include location_dim

--- cobradb_frontend/getautoname.php
+++ cobradb_frontend/getautoname.php
@@ -4,16 +4,26 @@
  
  $term=$_GET["term"];
  
- // TODO: will need to go through activity_fact to get location.........
- $query=mysql_query("SELECT surname,forename, street, city, state, country FROM person_dim, location_dim where surname like '%".$term."%' or forename like '%".$term."%' group by surname");
- $json=array();
  
+ $query=mysql_query("SELECT id_person_dim, surname, forename, id_activity_fact, fact_person, fact_location, id_location_dim, city, state, country 
+ FROM person_dim, activity_fact, location_dim
+ WHERE surname LIKE '%".$term."%' OR forename LIKE '%".$term."%' 
+ AND id_person_dim=fact_person 
+ AND id_location_dim=fact_location
+ GROUP BY surname");
+
+// TODO: include both person_dim AND location_dim in this while loop to display person and location data.........
+ 
+ $json=array();
     while($person_dim=mysql_fetch_array($query)){
+    while($location_dim=mysql_fetch_array($query)){
          $json[]=array(
-                    'value'=> $person_dim["surname"].", ".$person_dim["forename"].", ".$location_dim["street"].", ".$location_dim["city"].", ".$location_dim["state"].", ".$location_dim["country"],
-                    'label'=>$person_dim["surname"].", ".$person_dim["forename"].", ".$location_dim["street"].", ".$location_dim["city"].", ".$location_dim["state"].", ".$location_dim["country"]
+                    'value'=>$person_dim["surname"].", ".$person_dim["forename"].", ".$location_dim["city"].", ".$location_dim["state"].", ".$location_dim["country"],
+                    'label'=>$person_dim["surname"].", ".$person_dim["forename"].", ".$location_dim["city"].", ".$location_dim["state"].", ".$location_dim["country"]
                         );
-    }
+    
+ }   
+}
  
  echo json_encode($json);
  
